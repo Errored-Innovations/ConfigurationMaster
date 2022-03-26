6e871a40d750ab356e94f7bbefb43c22d81fe55f
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-03-26 18:27:00 +0000

    OH YEAH UH BUMP THIS PLS

--- API/src/main/java/io/github/thatsmusic99/configurationmaster/impl/CMConfigSection.java
+++ API/src/main/java/io/github/thatsmusic99/configurationmaster/impl/CMConfigSection.java
@@ -77,7 +77,7 @@ public class CMConfigSection extends CMMemorySection implements ConfigSection {
         Objects.requireNonNull(newPath, "The new path cannot be null!");
         Objects.requireNonNull(otherFile, "The file being transferred to cannot be null!");
 
-        if (!contains(oldPath, true)) return;
+        if (!contains(oldPath)) return;
         CMMemorySection oldCmSection = getSectionInternal(oldPath, false);
         if (oldCmSection == null) return;
         CMMemorySection newCmSection = otherFile.getSectionInternal(newPath);
