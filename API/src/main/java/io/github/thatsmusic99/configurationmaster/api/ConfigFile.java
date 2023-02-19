44271d84816b63262ced82d4210c5f645655c6d9
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-02-19 13:39:28 +0000

    so many changes in this file omg

--- API/src/main/java/io/github/thatsmusic99/configurationmaster/api/ConfigFile.java
+++ API/src/main/java/io/github/thatsmusic99/configurationmaster/api/ConfigFile.java
@@ -1,18 +1,22 @@
 package io.github.thatsmusic99.configurationmaster.api;
 
+import io.github.thatsmusic99.configurationmaster.annotations.Option;
+import io.github.thatsmusic99.configurationmaster.api.comments.Comment;
 import io.github.thatsmusic99.configurationmaster.impl.CMConfigSection;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.SafeConstructor;
 import org.yaml.snakeyaml.error.YAMLException;
 import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.*;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.*;
+import java.util.function.Function;
 import java.util.logging.Logger;
 
 /**
@@ -23,7 +27,7 @@ import java.util.logging.Logger;
  * <ol>
  *     <li>{@link ConfigFile#loadConfig(File)} - this loads a file with safety precautions.
  *     If the file contains a syntax error, the API will print an error,
- *     rename the file temporarily and load a new empty file.
+ *     rename the file temporarily and load a new empty file.<br>
  *
  *     It is recommended to use this if you want your users to
  *     not lose their progress on a config file if they make a
@@ -40,39 +44,311 @@ import java.util.logging.Logger;
  */
 public class ConfigFile extends CMConfigSection {
 
-    private final Yaml yaml;
-    private final DumperOptions yamlOptions = new DumperOptions();
-    private final Representer yamlRepresenter = new Representer();
-    private final File file;
-    private boolean isNew = false;
-    private final CommentWriter writer;
-    private Title title = null;
-    protected List<String> pendingComments;
-    protected HashMap<String, String> comments;
-    protected HashSet<String> examples;
-    protected List<String> lenientSections;
+    @NotNull private final Yaml yaml;
+    @NotNull protected final DumperOptions yamlOptions = new DumperOptions();
+    @NotNull protected final Representer yamlRepresenter = new Representer();
+    @NotNull private final File file;
+    @NotNull private final CommentWriter writer;
+    @NotNull protected List<Comment> pendingComments;
+    @NotNull protected HashMap<String, List<Comment>> comments;
+    @NotNull protected HashSet<String> examples;
+    @NotNull protected List<String> lenientSections;
+    @NotNull protected Function<String, String> optionNameTranslator;
+    @Nullable private Title title;
+    private boolean isNew;
     protected boolean verbose;
     protected static Logger logger = new CMLogger();
 
     /**
-     * Used to load a config file without safety precautions taken by the API.
+     * Used to initialise a config file without safety precautions taken by the API.
      *
      * @param file The config file to be loaded.
      * @see ConfigFile#loadConfig(File)
      * @throws YAMLException if the file being loaded contains syntax errors.
      */
-    public ConfigFile(@NotNull File file) throws IOException {
+    public ConfigFile(@NotNull File file) throws IOException, IllegalAccessException {
+        this(file, name -> name.replaceAll("_", "-").toLowerCase());
+    }
+
+    public ConfigFile(@NotNull File file, @NotNull Function<String, String> optionNameTranslator) throws IOException, IllegalAccessException {
+
+        // Load the YAML configuration
         yaml = new Yaml(new SafeConstructor(), yamlRepresenter, yamlOptions);
         yamlOptions.setIndent(2);
         yamlOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
         yamlRepresenter.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+        // Set up the file itself
         this.file = file;
+        this.optionNameTranslator = optionNameTranslator;
+        this.isNew = false;
+        this.title = null;
+
+        // Set up internal variables
         writer = new CommentWriter(this);
         pendingComments = new ArrayList<>();
         comments = new HashMap<>();
         examples = new HashSet<>();
         lenientSections = new ArrayList<>();
-        loadWithExceptions();
+    }
+
+    /**
+     * Used to load a config file with safety precautions taken by the API.<br>
+     * This safety precaution checks for syntax errors in the YAML file.<br>
+     * If there is one, it is renamed and an empty file is loaded.<br>
+     *
+     * @param file The file to be loaded.
+     * @return the ConfigFile instance of the file or backup file.
+     */
+    public static ConfigFile loadConfig(File file) throws Exception {
+        ConfigFile configFile = new ConfigFile(file);
+        configFile.load();
+        return configFile;
+    }
+
+    public void load() throws Exception {
+
+        // If the file doesn't already exist, create it
+        if (!file.exists()) {
+            if (!file.createNewFile()) {
+                throw new IOException("Failed to create " + file.getName() + "!");
+            }
+
+            // It's a new file
+            isNew = true;
+        }
+
+        // Read the file content
+        String content = readFileContent();
+
+        // Load any options from the content
+        loadFromString(content);
+
+        // Load the default options
+        addDefaults();
+
+        // Move everything to the new options
+        moveToNew();
+
+        // Then save!
+        save();
+
+        // Carry out any extra operations post-save
+        postSave();
+    }
+
+    protected void loadFromString(String content) throws IOException {
+        try {
+
+            // Load everything
+            Map<?, ?> map = this.yaml.load(content);
+            if (map != null) {
+                mapToCM(map);
+            }
+
+        } catch (YAMLException exception) {
+
+            File tempFile = new File(file.getParentFile(),
+                    file.getName().substring(0, file.getName().lastIndexOf("."))
+                            + "-errored-" + System.currentTimeMillis() + ".yml");
+
+            Files.copy(file.toPath(), tempFile.toPath());
+
+            logger.severe("YAMLException caught - there is a syntax error in the config.");
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Used to load options
+     */
+    public void addDefaults() throws Exception {
+
+        handleAnnotations((field, option) -> {
+
+            // Also get the field value and treat it as the default option
+            final Object defaultOpt = field.get(this);
+
+            // Get the option metadata
+            final String name = option.path().isEmpty() ? optionNameTranslator.apply(field.getName()) : option.path();
+            final String comment = option.comment().isEmpty() ? null : option.comment();
+            final String section = option.section().isEmpty() ? null : option.section();
+
+            // Add the default
+            addDefault(name, defaultOpt, section, comment);
+
+            // Set the result
+            field.set(this, get(name));
+        });
+    }
+
+    public void moveToNew() {
+    }
+
+    public void postSave() {
+    }
+
+    /**
+     * Saves all changes made to the config to the actual file.
+     *
+     * @throws IOException if something went wrong writing to the file.
+     */
+    public void save() throws Exception {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(file.toPath()), StandardCharsets.UTF_8))) {
+            String saved = saveToString();
+            writer.write(saved);
+            isNew = false;
+            this.existingValues.clear();
+        }
+    }
+
+    protected String readFileContent() throws IOException {
+
+        StringBuilder content = new StringBuilder();
+
+        // Load the reader
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(file.toPath()), StandardCharsets.UTF_8))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            if (content.length() == 0) {
+                isNew = true;
+            }
+        }
+
+        return content.toString();
+    }
+
+    /**
+     * Reloads the configuration and updates all values stored inside it.<br><br>
+     *
+     * @throws IOException if something went wrong saving the file.
+     */
+    public void reload() throws Exception {
+        debug("Reloading the configuration file " + file.getName() + "...");
+
+        // Reset the defaults
+        HashMap<String, Object> allDefaults = new LinkedHashMap<>();
+        addDefaults(allDefaults);
+
+        // Reset internal values
+        existingValues.clear();
+        clear();
+
+        // Reset content
+        String content = readFileContent();
+        loadFromString(content);
+        addDefaults();
+        moveToNew();
+
+        //
+        for (String path : allDefaults.keySet()) {
+            if (!examples.contains(path) || contains(path) || isNew) {
+                addDefault(path, allDefaults.get(path));
+            }
+        }
+
+        for (String section : lenientSections) {
+            makeSectionLenient(section);
+        }
+
+        save();
+        postSave();
+    }
+
+    /**
+     * Returns whether the loaded file is brand new or not.<br>
+     *
+     * This is determined by whether a new file was created or if the file itself is empty.
+     *
+     * @return true if the file is newly loaded, false if not.
+     */
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void updateAnnotations() throws Exception {
+
+        handleAnnotations((field, option) -> {
+
+            // Also get the field value and treat it as the default option
+            final Object value = field.get(this);
+
+            // Get the option metadata
+            final String name = option.path().isEmpty() ? optionNameTranslator.apply(field.getName()) : option.path();
+
+            // Update the values
+            set(name, value);
+        });
+    }
+
+    private void handleAnnotations(OptionConsumer<Field, Option> consumer) throws Exception {
+        // Before doing anything else, check the fields
+        Field[] fields = getClass().getFields();
+        for (Field field : fields) {
+
+            // Check if a field has the option annotation, add it
+            if (!field.isAnnotationPresent(Option.class)) continue;
+            final Option option = field.getAnnotation(Option.class);
+
+            //
+            consumer.accept(field, option);
+        }
+    }
+
+    public String saveToString() throws Exception {
+
+        // Update the annotations
+        updateAnnotations();
+
+        // Convert the map
+        String dump = this.yaml.dump(convertToMap());
+        if (dump.equals("{}")) dump = "";
+
+        // Write the comments
+        writer.writeComments(new ArrayList<>(Arrays.asList(dump.split("\n"))));
+
+        // Write to the lines
+        StringBuilder result = new StringBuilder();
+        writer.getLines().forEach(line -> result.append(line).append("\n"));
+
+        // Write the title and result
+        return (title != null ? title + "\n" : "") + result;
+    }
+
+    public @NotNull HashMap<String, List<Comment>> getComments() {
+        return comments;
+    }
+
+    /**
+     * Get all comments that have yet to be added. These will be added when the next default/example is set.
+     *
+     * @return Comments waiting to be added.
+     */
+    public @NotNull List<Comment> getPendingComments() {
+        return pendingComments;
+    }
+
+    public @NotNull HashSet<String> getExamples() {
+        return examples;
+    }
+
+    public @NotNull List<String> getLenientSections() {
+        return lenientSections;
+    }
+
+    public @Nullable Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(@Nullable Title title) {
+        this.title = title;
+    }
+
+    public @NotNull File getFile() {
+        return file;
     }
 
     /**
@@ -89,169 +365,16 @@ public class ConfigFile extends CMConfigSection {
         if (verbose) logger.info(message);
     }
 
-    /**
-     * Used to load a config file with safety precautions taken by the API.<br>
-     * This safety precaution checks for syntax errors in the YAML file.<br>
-     * If there is one, it is renamed and an empty file is loaded.<br>
-     *
-     * @param file The file to be loaded.
-     * @return the ConfigFile instance of the file or backup file.
-     */
-    public static ConfigFile loadConfig(File file) throws IOException {
-        try {
-            return new ConfigFile(file);
-        } catch (YAMLException e) {
-            File tempFile = new File(file.getParentFile(),
-                    file.getName().substring(0, file.getName().lastIndexOf("."))
-                            + "-errored-" + System.currentTimeMillis() + ".yml");
-
-            try {
-                if (!tempFile.exists()) {
-                    tempFile.createNewFile();
-                }
-                Files.copy(file.toPath(), tempFile.toPath());
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-            logger.severe("YAMLException caught - there is a syntax error in the config.");
-            e.printStackTrace();
-            return new ConfigFile(tempFile);
-        }
-    }
-
-    private void loadWithExceptions() throws IOException {
-        BufferedReader reader = null;
-        try {
-            try {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-            } catch (FileNotFoundException ex) {
-                file.createNewFile();
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-                isNew = true;
-            }
-
-            StringBuilder content = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-            if (content.length() == 0) {
-                isNew = true;
-            }
-            loadFromString(content.toString());
-        } finally {
-            if (reader != null) reader.close();
-        }
-    }
-
-    private void loadFromString(String str) {
-        Map<?, ?> map = this.yaml.load(str);
-        if (map != null) {
-            mapToCM(map);
-        }
-    }
-
-    /**
-     * Saves all changes made to the config to the actual file.
-     *
-     * @throws IOException if something went wrong writing to the file.
-     */
-    public void save() throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8))) {
-            String saved = saveToString();
-            writer.write(saved);
-            isNew = false;
-            this.existingValues.clear();
-        }
-    }
-
-    /**
-     * Reloads the configuration and updates all values stored inside it.<br><br>
-     *
-     * @throws IOException if something went wrong saving the file.
-     */
-    public void reload() throws IOException {
-        HashMap<String, Object> allDefaults = new LinkedHashMap<>();
-        addDefaults(allDefaults);
-        existingValues.clear();
-        actualValues.clear();
-        loadWithExceptions();
-
-        for (String path : allDefaults.keySet()) {
-            if (!examples.contains(path) || contains(path) || isNew) {
-                addDefault(path, allDefaults.get(path));
-            }
-        }
-
-        for (String section : lenientSections) {
-            makeSectionLenient(section);
-        }
-
-        save();
-    }
-
-    /**
-     * Returns whether the loaded file is brand new or not.
-     *
-     * This is determined by whether a new file was created or if the file itself is empty.
-     *
-     * @return true if the file is newly loaded, false if not.
-     */
-    public boolean isNew() {
-        return isNew;
-    }
-
-    public String saveToString() {
-        String dump = this.yaml.dump(convertToMap());
-        if (dump.equals("{}")) {
-            dump = "";
-        }
-        writer.writeComments(new ArrayList<>(Arrays.asList(dump.split("\n"))));
-        StringBuilder result = new StringBuilder();
-        for (String line : writer.getLines()) {
-            result.append(line).append("\n");
-        }
-        return (title != null ? title + "\n" : "") + result;
-    }
-
-    public HashMap<String, String> getComments() {
-        return comments;
-    }
-
-    /**
-     * Get all comments that have yet to be added. These will be added when the next default/example is set.
-     *
-     * @return Comments waiting to be added.
-     */
-    public List<String> getPendingComments() {
-        return pendingComments;
-    }
-
-    public HashSet<String> getExamples() {
-        return examples;
-    }
-
-    public List<String> getLenientSections() {
-        return lenientSections;
-    }
-
-    public Title getTitle() {
-        return title;
-    }
-
-    public void setTitle(Title title) {
-        this.title = title;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
     private static class CMLogger extends Logger {
 
         // if you can't be 'em, join 'em
         protected CMLogger() {
             super("ConfigurationMaster", null);
         }
+    }
+
+    private interface OptionConsumer<T, R> {
+
+        void accept(T t, R r) throws Exception;
     }
 }
